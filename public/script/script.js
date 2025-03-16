ac1c114c0e13055674db1a19806b83d879c9552a
Juancho Gutierrez <gutierrezgonzalezjuan05@gmail.com>
2025-03-15 21:34:47 -0600

    Se ha hecho la pagina 100% Responsive

--- public/script/script.js
+++ public/script/script.js
@@ -86,3 +86,9 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+const menuToggle = document.getElementById('menu-toggle');
+    const navMenu = document.getElementById('nav-menu');
+
+    menuToggle.addEventListener('click', () => {
+        navMenu.classList.toggle('show');
+    });
